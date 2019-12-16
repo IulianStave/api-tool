e96380a37b9f6ac9a5af2a9296431b5ccfb58010
iulianstave <iulian.staverescu@gmail.com>
2019-12-16 13:11:45 +0200

    Try to handle Index error exception

--- api.py
+++ api.py
@@ -23,41 +23,51 @@ def get_user_id(workspace_id, user_name):
             'X-Api-key': api_key,
         },
     )
-    # print(f'Getting users in workspace {workspace_id}... \
-    #     [Status code: {resp.status_code}]')
     if resp.status_code == 200:
         data = resp.json()
-        # print('Project entries in workspace {} for project {}'.format(workspace_id, project_name))
-        # result = [entry for entry in data if entry['name']==project_name][0]['id']
         try:
-            return [entry['id'] for entry in data if entry['name'] == user_name][0]
+            # return filter(lambda x: x['name'] == user_name, data)['id']
+            return [entry['id'] for entry in data
+                    if entry['name'] == user_name][0]
         except IndexError:
-            return 'Index Error'
-        # id = filter(lambda x: x['name'] == user_name, data)['id']
+            return f'Index Error: {user_name} not found'
 
 
 def get_project_id(workspace_id, project_name):
-    get_projects_path = f'/workspaces/{workspace_id}/projects'
-    URL = url_base + get_projects_path
+    PATH = f'/workspaces/{workspace_id}/projects'
+    URL = f'{url_base}{PATH}'
     resp = requests.get(
         url=URL,
         headers={
             'X-Api-key': api_key,
-        },
+        }
     )
     print(f'Reading projects... [Status code: {resp.status_code}]')
     if resp.status_code == 200:
         data = resp.json()
-        # print('Project entries in workspace {} for project {}'.format(workspace_id, project_name))
-        # result = [entry for entry in data if entry['name']==project_name][0]['id']
-        # id = [entry for entry in data if entry['name']==project_name][0]['id']
-        for entry in data:
-            if entry['name'] == project_name:
-                print('>> Project {} found, id: {}'.format(
-                    project_name, entry['id']))
-                return entry['id']
-        return f'Error, {project_name} not found in workspace {workspace_id}'
-        # retrun entry['id'] for entry in data if entry['name']==project_name
+        try:
+            return [entry['id'] for entry in data
+                    if entry['name'] == project_name][0]
+        except IndexError:
+            return f'Index Error: {project_name} not found'
+
+
+def get_workspace_id(workspace):
+    PATH = '/workspaces'
+    URL = f'{url_base}{PATH}'
+    resp = requests.get(
+        url=URL,
+        headers={
+            'X-Api-key': api_key,
+        }
+    )
+    if resp.status_code == 200:
+        data = resp.json()
+        try:
+            return [entry['id']
+                    for entry in data if entry['name'] == workspace][0]
+        except IndexError:
+            return f'Index Error: {workspace} not found'
 
 
 def add_time_entry(workspace_id, post_data):
@@ -71,9 +81,9 @@ def add_time_entry(workspace_id, post_data):
         },
         json=post_data
     )
-    print(f'>>>>> Success! New time entry on {workspace_id} has been created'
-          if resp_add.status_code == 201 else f'POST {PATH_ADD_TE} \
-              error code {resp_add.status_code}'
+    print(f'>>> New time entry on {workspace_id} has been created'
+          if resp_add.status_code == 201 
+          else f'POST {PATH_ADD_TE} error code {resp_add.status_code}'
           )
 
 
@@ -81,7 +91,7 @@ def delete_entry(workspace_id, entry_id):
     # DELETE /workspaces/{workspaceId}/time-entries/{id}
     PATH = f'/workspaces/{workspace_id}/time-entries/{entry_id}'
     URL = f'{url_base}{PATH}'
-    print(f'Delete entry URL: {URL} \n')
+    # print(f'Delete entry URL: {URL} \n')
     r = requests.delete(
         url=URL,
         headers={
@@ -95,15 +105,14 @@ def delete_entry(workspace_id, entry_id):
 
 def delete_entries(workspace_id, user_name, project_name):
     # GET /workspaces/{workspaceId}/user/{userId}/time-entries
-     # get time entries for the project, user
     user_id_source = get_user_id(workspace_id, user_name)
     page_number = 1
     ended = False
     while not ended:
-        GET_TIME_PATH = '/workspaces/{}/user/{}/time-entries/?page={}'.format(
+        PATH = '/workspaces/{}/user/{}/time-entries/?page={}'.format(
             workspace_id, user_id_source, page_number)
-        print(GET_TIME_PATH)
-        URL = url_base + GET_TIME_PATH
+        print(PATH)
+        URL = f'{url_base}{PATH}'
         resp = requests.get(
             url=URL,
             headers={
@@ -115,49 +124,20 @@ def delete_entries(workspace_id, user_name, project_name):
         counter = 0
         if resp.status_code == 200:
             data = resp.json()
-            project_id = get_project_id(
-                workspace_id, project_name)
-            
-
-            print(f'>>> {len(data)} Entries found. All projects')
+            total_entries = len(data)
+            project_id = get_project_id(workspace_id, project_name)
+            print(f'>>> {total_entries} Entries on page for all projects')
             for entry in data:
                 if entry['projectId'] == project_id:
                     counter += 1
-                    print(f'Entry found id {entry["id"]}')
-                    # value for start key is mandatory
-                    # new_entry['start'] = entry['timeInterval']['start']
-                    # new_entry['end'] = entry['timeInterval']['end']
-                    # new_entry['billable'] = entry['billable']
-                    # new_entry['description'] = entry['description']
-                    # new_entry['timeInterval'] = entry['timeInterval']
-                    # new_entry['projectId'] = project_id_dest
-                    # new_entry['userId'] = user_id_dest
+                    # print(f'Entry found id {entry["id"]}')
                     delete_entry(workspace_id, entry['id'])
-
             print(f'Counter: {counter}')
-            print(len(data))
-            if len(data) < 50:
+            if total_entries < 50:
                 ended = True
             else:
                 page_number += 1
-    print(page_number)
-
-
-def get_workspace_id(workspace_name):
-    # find workspaces for currently logged in user
-    PATH = '/workspaces'
-    URL = f'{url_base}{PATH}'
-    resp = requests.get(
-        url=URL,
-        headers={
-            'X-Api-key': api_key,
-        },
-    )
-    data = resp.json()
-    # and get the id of the already existing workspace identified by name
-    workspace_id_dest = [workspace['id']
-                            for workspace in data if workspace['name'] == workspace_name][0]
-    return workspace_id_dest
+    print(f'{page_number} pages parsed')
 
 
 def add_workspace(workspace_name):
@@ -171,7 +151,6 @@ def add_workspace(workspace_name):
         },
         json={'name': workspace_name}
     )
-
     if resp.status_code == 201:
         workspace_id = resp.json()["id"]
         print(
@@ -180,7 +159,7 @@ def add_workspace(workspace_name):
     if resp.status_code == 400:
         workspace_id = get_workspace_id(workspace_name)
         print(
-            f'Workspace \'{workspace_name}\' already exists, id: {workspace_id}'
+            f'Workspace \'{workspace_name}\' already exists Id: {workspace_id}'
         )
         return workspace_id
     else:
@@ -192,52 +171,47 @@ def read_config():
     config_path = default_config_json
     if len(sys.argv) > 1:
         config_path = sys.argv[1]
-    print(f'JSON configuration file is \'{config_path}\'')
+    print(f'JSON configuration file: \'{config_path}\'')
     if not path.exists(config_path):
         print(
-            f'Configuration JSON file \'{config_path}\' not found')
+            f'JSON configuration file \'{config_path}\' not found')
         print('It should follow the \'config.json\' structure')
         return 'Error'
     with (open(config_path)) as file:
         clockify_data = json.load(file)
     # unpack the keys from the dictionary to individual variables
-    print('Reading clockify data from file...')
+    print('Reading clockify data from configuration file...')
     for key, val in clockify_data.items():
-        exec('global '+key+'\n'+ key + '=val')
+        exec('global ' + key + '\n' + key + '=val')
         print(key, ' =  ', val)
     return clockify_data
 
 
-def copy_time_entries(
-                        workspace_id, user_name, project_name, 
-                        workspace_id_dest, user_name_dest, project_name_dest
-                        ):
+def copy_time_entries(workspace_id, user_name, project_name,
+                      workspace_id_dest, user_name_dest, project_name_dest):
     # get time entries for the project, user
     user_id_source = get_user_id(workspace_id, user_name)
     page_number = 1
     ended = False
     while not ended:
-        GET_TIME_PATH = '/workspaces/{}/user/{}/time-entries/?page={}'.format(
+        PATH = '/workspaces/{}/user/{}/time-entries/?page={}'.format(
             workspace_id, user_id_source, page_number)
-        print(GET_TIME_PATH)
-        URL = url_base + GET_TIME_PATH
+        URL = f'{url_base}{PATH}'
         resp = requests.get(
             url=URL,
             headers={
                 'X-Api-key': api_key,
             }
         )
-        print(
-            f'Get Time entries in workspace...[Status code: {resp.status_code}]')
+        print(f'Getting time entries...[Status code: {resp.status_code}]')
         counter = 0
         if resp.status_code == 200:
             data = resp.json()
+            total_entries = len(data)
+            print(f'>>> {total_entries} Entries found on page for all projects')
             project_id_source = get_project_id(
                 workspace_id, project_name_source)
-            user_id_dest = get_user_id(
-                workspace_id_dest, user_name_dest)
-
-            print(len(data))
+            user_id_dest = get_user_id(workspace_id_dest, user_name_dest)
             for entry in data:
                 if entry['projectId'] == project_id_source:
                     counter += 1
@@ -251,15 +225,12 @@ def copy_time_entries(
                     new_entry['projectId'] = project_id_dest
                     new_entry['userId'] = user_id_dest
                     add_time_entry(workspace_id_dest, new_entry)
-
             print(f'Counter: {counter}')
-            print(type(resp))
-            print(len(data))
-            if len(data) < 50:
+            if total_entries < 50:
                 ended = True
             else:
                 page_number += 1
-    print(page_number)
+    print(f'{page_number} pages parsed')
 
 
 def main():
@@ -272,20 +243,27 @@ if __name__ == "__main__":
     main()
 
 
-read_config()
-workspace_id_dest = add_workspace(workspace_name_dest)
-if workspace_id_dest == 'Error':
-    exit()
+# read_config()
+workspace_id_dest = get_workspace_id(workspace_name_dest)
+# workspace_id_dest = add_workspace(workspace_name_dest)
+# if workspace_id_dest == 'Error':
+#     exit()
 user_id_source = get_user_id(workspace_id, user_name)
 project_id_dest = get_project_id(workspace_id_dest, project_name_dest)
 print(f'User passed in json is {user_name} id: {user_id_source}')
-myuser = 'User name'
-# copy_time_entries(workspace_id, user_name, project_name_source,
-#                   workspace_id_dest, user_name_dest, project_name_dest) 
-print(f'>>>> Destination workspace {workspace_id_dest} \
-::: id: {get_user_id(workspace_id_dest, myuser)} name: {myuser}')
-# print('deleting entries')
-# delete_entries(workspace_id_dest, myuser, project_name_dest)
+myuser = 'Some name'
+print(f'Checking for user id: {get_user_id(workspace_id, myuser)}')
 # copy_time_entries(workspace_id, user_name, project_name_source,
 #                   workspace_id_dest, user_name_dest, project_name_dest)
-print(get_workspace_id('workspace name'))
+print(f'>>>> Destination workspace {workspace_id_dest} \
+::: user id: {get_user_id(workspace_id_dest, myuser)}')
+# user_del = 'Some name'
+user_del = 'Ion'
+print(f'Checking for user id: {get_user_id(workspace_id, user_del)}')
+delete_entries(workspace_id_dest, user_del, project_name_dest)
+# copy_time_entries(workspace_id, user_name, project_name_source,
+#                   workspace_id_dest, user_name_dest, project_name_dest)
+ws = 'Beta workspace'
+print(f'Checking for workspace {ws} id: {get_workspace_id(ws)}')
+# copy_time_entries(workspace_id, user_name, project_name_source,
+#                   workspace_id_dest, user_name_dest, project_name_dest)
